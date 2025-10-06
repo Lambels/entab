94a4d9f386dd8227b7eab59912fcc523e7780119
Lambels <patrick.arvatu@yahoo.com>
2025-10-06 11:55:41 +0200

    allowed pipelining

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
+#include "dentab.h"
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "dentab.h"
 
 void usage(char *);
 
@@ -70,21 +70,23 @@ int main(int argc, char *argv[]) {
 
   // if we dont have an instream set we treat the next arguments as
   // our instream.
-  if (!instream) {
+  if (!instream && argc > 1) {
     // expect only one argument after parsing options and no file stream
     // provided.
-    if (argc != 1) {
-      usage("Expected 1 text argument\n");
-      return 1;
-    }
+    usage("Expected only 1 text argument\n");
+    return 1;
 
-    // open a stream backed by the argument provided.
-    instream = fmemopen(argv[0], strlen(argv[0]), "r");
-    if (!instream) {
-      perror("Error while trying to open argument buffer stream");
-      return 1;
-    }
+  } else if (!instream && argc == 1) {
+      // open a stream backed by the argument provided.
+      instream = fmemopen(argv[0], strlen(argv[0]), "r");
+      if (!instream) {
+          perror("Error while trying to open argument buffer stream");
+          return 1;
+      }
+  } else if (!instream && argc == 0) {
+        instream = fdopen(STDIN_FILENO, "r");
   }
+
 
   // we write to stdout.
   if (!outstream) {
@@ -95,10 +97,11 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  return (ed == 1) ? entab(instream, outstream, t) : detab(instream, outstream, t);
+  return (ed == 1) ? entab(instream, outstream, t)
+                   : detab(instream, outstream, t);
 }
 
 void usage(char *ctx) {
   printf("%s", ctx);
-  printf("Usage: dentab {-e|-d} [-h] [-t tab] [-o file] {-f file | text}\n");
+  printf("Usage: dentab {-e|-d} [-h] [-t tab] [-o file] [-f file | text]\n");
 }
